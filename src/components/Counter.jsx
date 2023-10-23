13bd0294ef0b37427790681c9fc5da3a6305a5ed
DataCodeLiteracy <dataliteracy@icloud.com>
2023-10-23 10:16:49 +0900

    study: 좀 더 복잡한 로직을 처리하기 위해 action을 객체로 변경해서 활용하기
    2023.10.23

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,30 +1,38 @@
-import { useReducer } from 'react'
+import { useReducer, useState } from 'react'
 
 const Counter = () => {
+  const [number, setNumber] = useState(0)
+
   // 2. 은행(useReducer)은 고객의 요구사항의 타입에 맞는 서비스를 제공하기 위해 회계 직원(countReducer)를 통해 업무 처리
   const countReducer = (oldState, action) => {
-    if (action === 'DOWN') {
-      return oldState - 1
-    } else if (action === 'RESET') {
+    if (action.type === 'DOWN') {
+      // reducer는 순수함수여야 하기 때문에 oldState - number 처럼 number를 reducer 함수 내부에서 변경해서 외부에 영향을 받도록 작성할 수 없다.
+      return oldState - Number(action.number)
+    } else if (action.type === 'RESET') {
       return (oldState = 0)
-    } else if (action === 'UP') {
-      return oldState + 1
+    } else if (action.type === 'UP') {
+      return oldState + Number(action.number)
     }
   }
+
   // 3. countReducer가 반환한 새로운 상태는 count 변수에 의해 나타내어지고, 화면에 렌더링
   const [count, countDispatch] = useReducer(countReducer, 0)
 
   // 1. 은행(useReducer)에서 고객(이벤트(down, reset, up))은 창구직원(countDispatch)에게 요구사항을 전달
   const down = () => {
-    countDispatch('DOWN')
+    countDispatch({ type: 'DOWN', number: number })
   }
 
   const reset = () => {
-    countDispatch('RESET')
+    countDispatch({ type: 'RESET' })
   }
 
   const up = () => {
-    countDispatch('UP')
+    countDispatch({ type: 'UP', number: number })
+  }
+
+  const handleNumber = (e) => {
+    setNumber(e.target.value)
   }
 
   return (
@@ -32,6 +40,7 @@ const Counter = () => {
       <input type="button" value="-" onClick={down} />
       <input type="button" value="0" onClick={reset} />
       <input type="button" value="+" onClick={up} />
+      <input type="text" value={number} onChange={handleNumber} />
       <span>{count}</span>
     </>
   )
